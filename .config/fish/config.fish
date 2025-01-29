a28510eb67072a992e1966be5eca1128dcb51ca6
Mohammad Naufal Maulana <mnaufalmaulana84@gmail.com>
2025-01-29 20:38:43 +0700

    update aliases on fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,15 +7,15 @@ if status is-interactive
     starship init fish | source
 
     # Aliases
-    alias orphan "sudo pacman -Qtdq | sudo pacman -Rns -"
     alias ls "exa --icons"
     alias fish-reload "source ~/.config/fish/config.fish"
     alias fish-config "vim ~/.config/fish/config.fish"
     alias starship-config "vim ~/.config/starship.toml"
     alias xampp-start "sudo /opt/lampp/xampp start"
     alias xampp-stop "sudo /opt/lampp/xampp stop"
-    
-    # Set pipx path
+    alias bat "batcat"
+
+    # Set Path
     set -gx PATH (string replace "/usr/bin/pipx" "" -- $PATH)
-    set -gx EDITOR "vim"
+    set -gx EDITOR "nvim"
 end
